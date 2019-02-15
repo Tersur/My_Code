197bd79e77bc93a471478e5f8b5e11b1b7ff3a46
Basil Tersur Gaji <44913748+Tersur@users.noreply.github.com>
2019-02-14 23:24:38 -0500

    Add files via upload

--- hw3_1_Gaji_Basil.c
+++ hw3_1_Gaji_Basil.c
@@ -1,14 +1,16 @@
 //hw3_1_Gaji_Basil.c by Basil Gaji
+
+
 #include<stdio.h>
 #define RATE_m .15
 #define RATE_l .20
 
 
 //a program to make an order at McDonald's
- int main(void){
+int main(void){
 
 	float b_mac = 6.99, g_a_chick = 7.49, mcnug = 6.99, chic_crisp = 7.79, b_s_art = 7.89, m_swiss = 8.69, size1, item_price, price, total;
-	int sel_item, amount, status;
+	int selected_item, amount, status;
 	char size, repeat;
 
 	//printing all selections	
@@ -21,101 +23,100 @@
 	printf("6.Mushroom Swiss\n");
 	printf("***************************\n");
 
+	total = 0;    																					//initializing the variable for storing the total price of selections after each round of selection
+	
 	printf("Select your order from #1~6: ");
+	selected_item = getchar();	  
+											
+	do{
 
-	total = 0;				//initializing the variable for storing the total price of selections after each round of selections
-	
-	do
-	{
-
-	    scanf("%d",&sel_item);
-	
-	    while ((sel_item >6)||(sel_item<1)){			//making sure the slected item ranges from 1 to 6
-	     
-	    while (getchar()!='\n');					//clearing the buffer
-	    printf("Please input a valid order number: ");
-                	scanf("%d",&sel_item);	      
-		}
-
-		//The different slections and their prices
+		while (((selected_item >54)||(selected_item<49))){				//making sure the slected item ranges from 1 to 6 by comparing ASCII codes
+			while (getchar()!='\n'){}																//clearing the buffer
+			printf("Please input a valid order number: ");
+	 		selected_item = getchar();
 		
-	   switch(sel_item){
-		case 1:
-			printf("You select: Big Mac, unit price: $6.99\n");
-			item_price = b_mac;					//the price to be used for calculating the selection
-			break;
-		case 2:
-			printf("You select: Grilled Asian Chicken, unit price: $7.49\n");
-			item_price = g_a_chick;
-			break;
-		case 3:
-			printf("You select: McNuggets, unit price: $6.99\n");
-			item_price = mcnug;
-			break;
-		case 4:
-			printf("You select: Chicken-Crispy, unit price: $7.79\n");
-			item_price = chic_crisp;
-			break;
-		case 5:
-			printf("You select: Bacon Smokehouse Artisan, unit price: $7.89\n");
-			item_price = b_s_art;
-			break;
-		case 6:
-			printf("You select: Mushroom Swiss, unit price: $8.69\n");
-			item_price = m_swiss;
-			break;
-	}
+	  	}  
+	
 
+	//The different slections and their prices
+		switch(selected_item){
+			case 1:case 49:																					//the second case makes use of the ASCII codes
+				printf("You select: Big Mac, unit price: $6.99\n");
+				item_price = b_mac;																		//the price to be used for calculating the selection
+				break;
+			case 2:case 50:
+				printf("You select: Grilled Asian Chicken, unit price: $7.49\n");
+				item_price = g_a_chick;
+				break;
+			case 3:case 51:
+				printf("You select: McNuggets, unit price: $6.99\n");
+				item_price = mcnug;
+				break;
+			case 4:case 52:
+				printf("You select: Chicken-Crispy, unit price: $7.79\n");
+				item_price = chic_crisp;
+				break;
+			case 5:case 53:
+				printf("You select: Bacon Smokehouse Artisan, unit price: $7.89\n");
+				item_price = b_s_art;
+				break;
+			case 6: case 54:
+				printf("You select: Mushroom Swiss, unit price: $8.69\n");
+				item_price = m_swiss;
+				break;
+			}
 
-	    printf("Input the amount: ");
-	    status = scanf("%d", &amount);					//makes sure the an integer is read
-		
-	    while (status != 1){
-	        while(getchar() != '\n'){
+		//entering the amount of selected items wanted
+		printf("Input the amount: ");
+
+	  status = scanf("%d", &amount);					
+	  while (status != 1){																		//makes sure the an integer is read
+	    while(getchar() != '\n'){}
 		  printf("Please input a valid order amount: ");
-		}
 		  status = scanf("%d", &amount);
 		
-		}
+			}
+	    
+		//entering the size of the selected items
+		printf("Input lower case s/m/l to select size: ");
+		getchar();
+	  size = getchar();  
+	  while(!((size == 's') || (size =='m') || (size =='l'))){
+			while(getchar()!='\n'){}
+			printf("Please input a valid size(s,m,l): ");
+			size = getchar();
+			}
 
-	   printf("Input lower case s/m/l to select size: ");		
-	   getchar();
-	   status = scanf("%c", &size);
-	   while (status != 1){
-	//	while (getchar() != 's'||'m'||'l'); 
-	   	while(getchar() != '\n');
-	    	printf("Please input a valid size(s,m,l): ");
-		 
-		 status =  scanf("%c", &size);        	
-	   }
-	   switch(size){
-	       case 's':
-			printf("You select small.\n");
-			size1 = 1;
-			break;
-	       case 'm':
-			printf("You select medium.\n");
-			size1 = 1 + RATE_m;
-			break;
-	       case 'l':
-			printf("You select large.\n");
-			size1 = 1 + RATE_l;
-			break;
-		}
+		//the different sizes of selected items
+	  switch(size){
+	    case 's':
+				printf("You select small.\n");
+				size1 = 1;																					//rate applied to the size selected
+				break;
+	    case 'm':
+				printf("You select medium.\n");
+				size1 = 1 + RATE_m;
+				break;
+	    case 'l':
+				printf("You select large.\n");
+				size1 = 1 + RATE_l;
+				break;
+				}
 				
-	  price = amount*item_price*size1;
-	  total += price;
+	  price = amount*item_price*size1;											//calculation of price selected items
+	  total += price;																				//increments the price after every order
 
 	  printf("Press c to continue or any other key to checkout. ");
-		
-	  getchar();
-		
+	 	getchar();	
 	  scanf("%c", &repeat);
+	  while(getchar()!= '\n'){}
 	  if (repeat == 'c'){
-		printf("Please select another item. ");
-		}
+			printf("Please select another item. ");
+			selected_item = getchar();
+			
+	  }
 
-      }while (repeat == 'c');
+  }while (repeat == 'c');
 
 	printf("Your total is $%.2f. Enjoy your meal!\n", total);
 
--- hw3_2_Gaji_Basil.c
+++ hw3_2_Gaji_Basil.c
@@ -1,33 +1,58 @@
 //hw3_2_Gaji_Basil.c by Basil Gaji
+
+
 #include<stdio.h>
 #include<stdbool.h>
+#include<stdlib.h>
 
 
 //a program to play rock paper scissors with the computer
  int main(void){
-	int your_ges;
+	int your_ges, comp_ges;
 	
 
 	while(true){					//the program runs continuously unless interrupted	
 	printf("Input your gesture (0-Rock 1-Paper 2-Scissors):\n");
-	scanf("%d", &your_ges);
-	
 
+	scanf("%d", &your_ges);
+
+	//printing your gesture
 	switch(your_ges){
 
 		case 0:
-			printf("Your gesture: Rock; Computer gesture: Rock.\n");
-			printf("Draw!\n");
+			printf("Your gesture: Rock; ");
 			break;
 		case 1:
-			printf("Your gesture: Paper; Computer gesture: Scissors.\n");
-			printf("You lose\n");
+			printf("Your gesture: Paper; ");
 			break;
 		case 2:
-			printf("Your gesture: Scissors; Computer gesture: Scissors\n");
-			printf("Draw!\n");
+			printf("Your gesture: Scissors; ");
 			break;
 
+	}
+
+	comp_ges = rand()%3;					//generating random numbers from 0 to 2
+	
+	//printing the computers gesture
+	switch(comp_ges){
+
+		case 0:
+			printf("Computer gesture: Rock.\n");
+			break;
+		case 1:
+			printf("Computer gesture: Paper.\n");
+			break;
+		case 2:
+			printf("Computer gesture: Scissors.\n");
+			break;
+	}
+
+	//printing whether you win or lose
+	if (your_ges == comp_ges){
+		printf("Draw!\n");
+		}
+	else{
+		printf("You lose!\n");
 	}
 	}
   return 0;
